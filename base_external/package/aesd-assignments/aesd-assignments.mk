9e7d9ed4841b55010cc4ff6331eb4ebc2f2305d9
bwaggle <bwaggle@Ubuntu.myguest.virtualbox.org>
2023-09-19 05:53:33 -0500

    Updated make versions

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 44d629488082a97a9b6c792ef00dd8efe1f747a9
+AESD_ASSIGNMENTS_VERSION = 032e41b2ab299672fbd077917e3f0584adf74777
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
